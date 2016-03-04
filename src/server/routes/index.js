3110503f813754c45dde5b807e74934a460cad50
David Sudia <dsudia@gmail.com>
2016-03-04 14:30:34 -0700

    error message correctly displays

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -3,8 +3,12 @@ var router = express.Router();
 var pg = require('pg');
 var knex = require('knex');
 
+var email = [
+  'dave@gmail.com',
+];
+
 router.get('/', function(req, res, next) {
-  res.render('index');
+  res.render('index', {messages: req.flash('danger')});
 });
 
 router.post('/', validEmail, function(req, res, next) {
@@ -13,7 +17,8 @@ router.post('/', validEmail, function(req, res, next) {
 
 function validEmail(req, res, next) {
   if (email.indexOf(req.body.email === -1)) {
-      res.send('you fucked up!');
+    req.flash('danger', 'Invalid email, please try again');
+    res.redirect('/');
   } else {
     return next();
   }
